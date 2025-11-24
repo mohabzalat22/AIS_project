d41ba1ee71c2bce65985643f8c64890fbefddcf8
mohab ali <mohabzalat20@gmail.com>
2025-11-24 16:36:14 +0200

    fix: fixing version issue for node.js

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom'
+
+// Polyfill for global
+declare const global: typeof globalThis;
+if (typeof global === 'undefined') {
+  Object.assign(globalThis, { global: globalThis });
+}
